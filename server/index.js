9f364748aecb5fee78873c56d50aa802c74aa3d4
adra3n <srtc.kcgl@gmail.com>
2023-08-07 20:15:21 +0300

    pg crud get, update

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const port = 5000
 app.use(cors())
 app.use(express.json())
 
-//////////routes/////////////////
+////////////routes///////////////
 
 //create
 
@@ -41,11 +41,35 @@ app.get('/todos', async (req, res) => {
 
 // get id
 
+app.get('/todos/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+    const todo = await pool.query('SELECT * FROM todo WHERE todo_id = $1', [id])
+    res.json(todo.rows[0])
+  } catch (error) {
+    console.log(error.message)
+  }
+})
+
 //update
+
+app.put('/todos/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+    const { description } = req.body
+    const updateTodo = await pool.query(
+      'UPDATE todo SET description = $1 WHERE todo_id= $2',
+      [description, id]
+    )
+    res.json(`ToDo #${id} has been updated`)
+  } catch (error) {
+    console.log(error.message)
+  }
+})
 
 //delete
 
-/////////////////////////////////////
+///////////////////////////
 
 app.listen(port, () => {
   console.log('server has started on port 5000')
