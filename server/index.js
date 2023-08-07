d150c4e18c8acadad2843a4625df031e0f0a047f
adra3n <srtc.kcgl@gmail.com>
2023-08-07 20:13:33 +0300

    pg crud, create, get ops

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const cors = require('cors')
+const pool = require('./db')
+
 const app = express()
 
 const port = 5000
@@ -7,6 +9,43 @@ const port = 5000
 //middlewares
 app.use(cors())
 app.use(express.json())
+
+//////////routes/////////////////
+
+//create
+
+app.post('/todos', async (req, res) => {
+  try {
+    const { description } = req.body
+    const newTodo = await pool.query(
+      'INSERT INTO todo (description) VALUES($1)',
+      [description]
+    )
+    res.json(newTodo.rows[0])
+    console.log(req.body)
+  } catch (error) {
+    console.log(error.message)
+  }
+})
+
+// get all
+
+app.get('/todos', async (req, res) => {
+  try {
+    const allTodos = await pool.query('SELECT * FROM todo')
+    res.json(allTodos.rows)
+  } catch (error) {
+    console.log(error.message)
+  }
+})
+
+// get id
+
+//update
+
+//delete
+
+/////////////////////////////////////
 
 app.listen(port, () => {
   console.log('server has started on port 5000')
